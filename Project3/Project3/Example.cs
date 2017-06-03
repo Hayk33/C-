cfb3069cee911f953dad0a87e9aaf75bd74323e5
Hayk33 <haykharut3@gmail.com>
2017-06-03 15:50:02 +0400

    Calculator on c#

---
+++ Project3/Project3/Example.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class integer
+{
+
+    static void Main()
+    {
+    int x,y,z;
+
+    Console.WriteLine("Գրեք առաջին թիվը");
+    x =Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine();
+    Console.WriteLine("ընտրեք գործողությունը\n1 +\n2 -\n3 * \n4 /");
+    z = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine();
+    Console.WriteLine("Գրեք առաջին թիվը");
+    y = Convert.ToInt32(Console.ReadLine());
+    switch (z)
+    {
+        case 1:
+        Console.WriteLine(x + " + " + y + " = " + +(x + y));
+        break;
+        case 2:
+        Console.WriteLine(x + " - " + y + " = " + +(x - y));
+        break;
+        case 3:
+        Console.WriteLine(x + " * " + y + " = " + +(x * y));
+        break;
+        case 4:
+        Console.WriteLine(x + " / " + y + " = " + +(x / y));
+        break;
+    }
+    }
+
+}
